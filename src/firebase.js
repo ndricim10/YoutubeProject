120849d9327081e92636820f0c9f4d4fe89f8bcc
Ndricim Jahaj <jahajndricim@gmail.com>
2022-06-18 17:20:11 +0300

    setting up

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVegnQVKQWDF8DhO5a6w9C9mSwUmglT10",
+    authDomain: "project-react-981c8.firebaseapp.com",
+    projectId: "project-react-981c8",
+    storageBucket: "project-react-981c8.appspot.com",
+    messagingSenderId: "839142463463",
+    appId: "1:839142463463:web:e86710f073ced17448cf42",
+    measurementId: "G-1GP3YY81ZN"
+  };
