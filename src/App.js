52914d4bf62aca375f10fbbb65c057bb79018643
Ndricim Jahaj <jahajndricim@gmail.com>
2022-06-20 16:43:59 +0300

    setup project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react'
-import './app.css'
-import Header from './Header/header'
+import React from 'react'
+import VideoCard from './Components/VideoCard/VideoCard'
+import Header from './Components/Header/Header'
+import Categories from './Components/Categories/categories'
+import Sidebar from './Components/Sidebar/sidebar'
 
-export default function App(){
-  
-    return (
-        <>
-        <div>
-            <Header />
-        </div>
-        </>
-    )
+export default function App() {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
 }
