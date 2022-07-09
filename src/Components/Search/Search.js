c37eb29bb39618e7989bc5b57d0509245569c9bc
Ndricim Jahaj <jahajndricim@gmail.com>
2022-07-09 17:35:47 +0300

    Search videos by keyword - finished

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,6 +4,7 @@ import SearchVideo from "./SearchVideo/SearchVideo";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getSearchedVideos } from "../../Redux/Actions/videosAction";
+import moment from "moment";
 
 export default function Search() {
   const { query } = useParams();
@@ -20,7 +21,15 @@ export default function Search() {
         results.map((video, i)=>{
           return(
             <SearchVideo
-          title={video.snippet?.title}
+            
+            title={video.snippet?.title}
+          published={moment(video.snippet?.publishedAt).startOf("day").fromNow()}
+          description={video.snippet?.description}
+          img={video.snippet?.thumbnails?.default?.url}
+          channelTitle={video.snippet?.channelTitle}
+          videoId={video.id?.videoId}
+          video={video}
+          channelId={video.snippet?.channelId}
           />
           )
         })
--- src/Components/Search/SearchVideo/SearchVideo.js
+++ src/Components/Search/SearchVideo/SearchVideo.js
@@ -1,46 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchVideo.scss";
 import "../../../index.scss";
 import "../../VideoCard/VideoCard.scss";
-import millify from "millify";
+import request from "../../../api";
+import moment from "moment";
+import numeral from "numeral";
+import {Link} from 'react-router-dom'
 
+export default function SearchVideo({
+  title,
+  videoId,
+  img,
+  published,
+  description,
+  channelTitle,
+  video,
+  channelId
+}){
 
-export default function SearchVideo({title}) {
+  const {
+    id,
+    contentDetails,
+  } = video;
+
+  const [views, setViews] = useState(null);
+const [duration, setDuration] = useState(null);
+const [channelIcon, setChannelIcon] = useState(null);
+
+ useEffect(() => {
+    const get_video_detail = async () => {
+      const {
+        data: { items },
+      } = await request("/channels", {
+        params: {
+          part: "snippet",
+          id: channelId,
+        },
+      });
+      setChannelIcon(items[0].snippet?.thumbnails?.default);
+    };
+    get_video_detail();
+  }, [channelId]);
+
+  const seconds = moment.duration(duration).asSeconds();
+  const _duration = moment.utc(seconds * 1000).format("mm:ss");
+
+  const _videoId = id?.videoId || contentDetails?.videoId || id;
+  useEffect(() => {
+    const get_video_details = async () => {
+      const {
+        data: { items },
+      } = await request("/videos", {
+        params: {
+          part: "contentDetails,statistics",
+          id: _videoId,
+        },
+      });
+      setDuration(items[0].contentDetails.duration);
+      setViews(items[0].statistics.viewCount);
+    };
+    get_video_details();
+  }, [_videoId]);
+
   return (
+    <Link className="a" to={`/watch/${_videoId}`}>
     <div className="search_video">
       <div className="video">
         <div className="video_top">
-          <img src="https://cdn.pixabay.com/photo/2017/10/10/21/49/youtuber-2838945_960_720.jpg" />
-          <span>05:20</span>
+          <img src={img} />
+          <span>{_duration}</span>
         </div>
         <div className="video_right">
           <div className="video_channel_title">
-            <span>
-              {title}
-            </span>
+            <span>{title}</span>
             <div className="video_details">
-              <span> {millify(3634446373)} views</span>
+              <span> {numeral(views).format('0.a')} views</span>
               <li>
-                <span>5 days ago</span>
+                <span>{published}</span>
               </li>
             </div>
           </div>
 
           <div className="video_channel">
-            <img src="https://yt3.ggpht.com/ypqAbDWiOc718gMoxAYbhA8brU_JZN1_V2qQBW1a-DvAyZaUGd2HnxiTl2mhe_KFUl_TvGPk=s68-c-k-c0x00ffffff-no-rj" />
-            <span>Ndricim</span>
+            <img src={channelIcon?.url}/>
+            <span>{channelTitle}</span>
           </div>
           <div className="video_description">
-           <span>
-           Perpunoi & Ork.: Vani Rushiti Klarinete: Maldin Avduli Kitare:
-            Xhuljo Imeri Audio: Eran Production Video: Malespi Pro Connect
-            Perpunoi & Ork.: Vani Rushiti Klarinete: Maldin Avduli Kitare:
-            Xhuljo Imeri Audio: Eran Production Video: Malespi Pro Connect ...
-          
-           </span>
-           </div>
+            <span>{description}</span>
+          </div>
         </div>
       </div>
     </div>
+    </Link>
   );
 }
